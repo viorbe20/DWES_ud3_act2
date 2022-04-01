ca490efecf83f5f68c3291212ba85bbc6493ffba
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2022-04-01 17:56:00 +0200

    feat: enunciado ej4

---
+++ ej4.php
@@ -0,0 +1,25 @@
+<!--Ejercicio 4.  Notas académicas.
+A partir de un array que almacene los datos de la primera y segunda evaluación de los alumnos de 2o DAW, 
+mostrar un menú de navegación que muestre la siguiente información. 
+• Listados de alumnos con las notas de la primera y segunda evaluación, junto con su nota media. 
+Asignatura con mayor número de aprobados. Asignatura con mayor número de suspensos. 
+Número de aprobados en cada asignatura. 
+Generación de boletines de notas en función de la evaluación seleccionada en una lista desplegable.
+Virginia Ordoño Bernier
+-->
+<?php
+
+?>
+<!--Primer formulario-->
+<form action="" method="post">
+    <h1>Ejercicio 4</h1>
+    <h2>Pulsa cualquier botón para ver la diferente información. </h2><br>
+    <ol>
+    <li>Listados de alumnos con las notas de la primera y segunda evaluación, junto con su nota media. <button type="submit1"> Mostrar</button></li><br>
+    <li>Asignatura con mayor número de aprobados. <button type="submit2"> Mostrar</button></li><br>
+    <li>Asignatura con mayor número de suspensos. <button type="submit3"> Mostrar</button></li><br>
+    <li>Número de aprobados en cada asignatura.  <button type="submit4"> Mostrar</button></li><br>
+    <li>Generación de boletines de notas en función de la evaluación seleccionada en una lista desplegable.  <button type="submit5"> Mostrar</button></li>
+    
+</ol>
+</form>
