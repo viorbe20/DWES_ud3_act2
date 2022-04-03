a4cbe7eba11b07391f3ee3d0815dfb4f8624c801
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2022-04-03 11:57:21 +0200

    feat: añade asignatura con más aprobados

--- ej4.php
+++ ej4.php
@@ -8,78 +8,113 @@ Generación de boletines de notas en función de la evaluación seleccionada en 
 Virginia Ordoño Bernier
 -->
 <?php
+$modules = array(
+    "DWES" => array(
+        "Andrea Solís Tejada" => array(0, 0),
+        "Carlos Chávez Hernández"  => array(0, 0),
+        "Daniel Ayala Cantador" => array(0, 0)
+    ),
+    "DWEC" => array(
+        "Andrea Solís Tejada" => array(2, 6),
+        "Carlos Chávez Hernández"  => array(5.8, 6),
+        "Daniel Ayala Cantador" => array(2, 2)
+    ),
+    "DIW" => array(
+        "Andrea Solís Tejada" => array(5, 5),
+        "Carlos Chávez Hernández"  => array(9, 3),
+        "Daniel Ayala Cantador" => array(2, 2)
+    ),
+    "DAW" => array(
+        "Andrea Solís Tejada" => array(4, 7.5),
+        "Carlos Chávez Hernández"  => array(8, 10),
+        "Daniel Ayala Cantador" => array(6, 4)
+    ),
+    "EIE" => array(
+        "Andrea Solís Tejada" => array(3, 10),
+        "Carlos Chávez Hernández"  => array(5.8, 6),
+        "Daniel Ayala Cantador" => array(6, 5)
+    ),
+    "HLC" => array(
+        "Andrea Solís Tejada" => array(3, 5),
+        "Carlos Chávez Hernández"  => array(5, 5),
+        "Daniel Ayala Cantador" => array(5, 6)
+    ),
+);
+
+foreach ($modules as $key => $value) {
+}
 $students = array(
     1 => array(
         "Nombre" => "Andrea Solís Tejada",
-        "DWES" => array(6, 7),
-        "DWEC" => array(8, 9),
+        "DWES" => array(4, 7),
+        "DWEC" => array(5, 9),
         "DIW" => array(10, 6),
-        "DIAW" => array(7, 8),
+        "DIAW" => array(7, 2.5),
         "HLC" => array(9, 10),
         "EIE" => array(6, 7),
     ),
     2 => array(
         "Nombre" => "Carlos Chávez Hernández",
-        "DWES" => array(6, 7),
-        "DWEC" => array(8, 9),
-        "DIW" => array(10, 6),
+        "DWES" => array(6, 3),
+        "DWEC" => array(3, 6),
+        "DIW" => array(10, 6.5),
         "DIAW" => array(7, 8),
-        "HLC" => array(9, 10),
+        "HLC" => array(9, 4),
         "EIE" => array(6, 7),
     ),
     3 => array(
         "Nombre" => "Daniel Ayala Cantador",
-        "DWES" => array(8, 9),
-        "DWEC" => array(10, 6),
+        "DWES" => array(8, 3),
+        "DWEC" => array(10, 3),
         "DIW" => array(7, 8),
-        "DIAW" => array(9, 10),
-        "HLC" => array(6, 7),
+        "DAW" => array(9.5, 10),
+        "HLC" => array(4, 7),
         "EIE" => array(8, 9),
     ),
     4 => array(
         "Nombre" => "David Pérez Ruiz",
         "DWES" => array(10, 6),
-        "DWEC" => array(7, 8),
+        "DWEC" => array(2, 8),
         "DIW" => array(9, 10),
-        "DIAW" => array(6, 7),
+        "DAW" => array(6, 7),
         "HLC" => array(8, 9),
         "EIE" => array(10, 6),
     ),
     5 => array(
         "Nombre" => "Javier Cebrián Muñoz",
-        "DWES" => array(7, 8),
-        "DWEC" => array(8, 9),
+        "DWES" => array(7.5, 8),
+        "DWEC" => array(4, 9),
         "DIW" => array(10, 6),
-        "DIAW" => array(7, 8),
+        "DAW" => array(4, 8),
         "HLC" => array(9, 10),
-        "EIE" => array(6, 7),
+        "EIE" => array(6, 2),
     ),
     6 => array(
         "Nombre" => "Jesús Díaz Rivas",
-        "DWES" => array(6, 7),
+        "DWES" => array(3.5, 7),
         "DWEC" => array(8, 9),
-        "DIW" => array(10, 6),
-        "DIAW" => array(7, 8),
-        "HLC" => array(9, 10),
+        "DIW" => array(10, 4),
+        "DAW" => array(4, 8),
+        "HLC" => array(9.5, 10),
         "EIE" => array(6, 7),
     ),
     7 => array(
         "Nombre" => "Rubén Ramírez Ribera",
-        "DWES" => array(8, 9),
+        "DWES" => array(8.5, 9),
         "DWEC" => array(10, 6),
-        "DIW" => array(7, 8),
-        "DIAW" => array(9, 10),
-        "HLC" => array(6, 7),
+        "DIW" => array(4, 8),
+        "DAW" => array(9, 10),
+        "HLC" => array(6, 3),
         "EIE" => array(8, 9),
     ),
     8 => array(
         "Nombre" => "Virginia Ordoño Bernier",
-        "DWES" => array(10, 6),
+        "DWES" => array(10, 4),
         "DWEC" => array(7, 8),
         "DIW" => array(9, 10),
-        "DIAW" => array(6, 7),
+        "DAW" => array(3.5, 7),
         "HLC" => array(8, 9),
-        "EIE" => array(10, 6),
+        "EIE" => array(6, 6),
     ),
 );
 $procesaBoton1 = false;
@@ -87,16 +122,108 @@ $procesaBoton2 = false;
 $procesaBoton3 = false;
 $procesaBoton4 = false;
 $procesaBoton5 = false;
-$color = "color"; 
 $media = "";
+$dwesNotas = array();
+$dwecNotas = array();
+$diwNotas = array();
+$dawNotas = array();
+$hlcNotas = array();
+$eieNotas = array();
+//Carga nombres de los módulos con más aprobados
+$highestMarksArray = array();
+//Carga el número de aprobados máximo
+$highestMark = 0;
+
+
+/**
+ * Dados un array con notas y el nombre del módulo, devuelve un array con los aprobados
+ */
+function cargaAprobados($marksArray, $moduleName)
+{
+    $aprobadosArray = array();
+
+    foreach ($marksArray[$moduleName] as $value) {
+        for ($i = 0; $i < count($value); $i++) {
+            if ($value[$i] >= 5) {
+                $aprobadosArray[] = $value[$i];
+            }
+        }
+    }
+    return $aprobadosArray;
+}
+
+
 
 if (isset($_POST['submit1'])) {
     $procesaBoton1 = true;
 }
 
+//Muestra la asignatura con más aprobados
 if (isset($_POST['submit2'])) {
     $procesaBoton2 = true;
-    $color = "color";
+
+    //Cargamos arrays con aprobados y contamos
+    $dwesNotas = count(cargaAprobados($modules, "DWES"));
+    $dwecNotas = count(cargaAprobados($modules, "DWEC"));
+    $diwNotas = count(cargaAprobados($modules, "DIW"));
+    $dawNotas = count(cargaAprobados($modules, "DAW"));
+    $hlcNotas = count(cargaAprobados($modules, "HLC"));
+    $eieNotas = count(cargaAprobados($modules, "EIE"));
+
+    //Comparamos números de aprobados
+    //Carga nombres de los módulos con más aprobados
+    if ($dwesNotas > $highestMark) {
+        //Actualizamos número y array y añadimos el nombre del módulo
+        $highestMark = $dwesNotas;
+        $highestMarksArray = array();
+        $highestMarksArray[] = "DWES";
+    } else if ($dwesNotas == $highestMark) {
+        $highestMarksArray[] = "DWES";
+    }
+
+    if ($dwecNotas > $highestMark) {
+        //Actualizamos número y array y añadimos el nombre del módulo
+        $highestMark = $dwecNotas;
+        $highestMarksArray = array();
+        $highestMarksArray[] = "DWEC";
+    } else if ($dwecNotas == $highestMark) {
+        $highestMarksArray[] = "DWEC";
+    }
+
+    if ($diwNotas > $highestMark) {
+        //Actualizamos número y array y añadimos el nombre del módulo
+        $highestMark = $diwNotas;
+        $highestMarksArray = array();
+        $highestMarksArray[] = "DIW";
+    } else if ($diwNotas == $highestMark) {
+        $highestMarksArray[] = "DIW";
+    }
+
+    if ($dawNotas > $highestMark) {
+        //Actualizamos número y array y añadimos el nombre del módulo
+        $highestMark = $dawNotas;
+        $highestMarksArray = array();
+        $highestMarksArray[] = "DAW";
+    } else if ($dawNotas == $highestMark) {
+        $highestMarksArray[] = "DAW";
+    }
+
+    if ($hlcNotas > $highestMark) {
+        //Actualizamos número y array y añadimos el nombre del módulo
+        $highestMark = $hlcNotas;
+        $highestMarksArray = array();
+        $highestMarksArray[] = "HLC";
+    } else if ($hlcNotas == $highestMark) {
+        $highestMarksArray[] = "HLC";
+    }
+    if ($eieNotas > $highestMark) {
+        //Actualizamos número y array y añadimos el nombre del módulo
+        $highestMark = $eieNotas;
+        $highestMarksArray = array();
+        $highestMarksArray[] = "EIE";
+    } else if ($eieNotas == $highestMark) {
+        $highestMarksArray[] = "EIE";
+    }
 }
 
 if (isset($_POST['refresh'])) {
@@ -106,11 +233,6 @@ if (isset($_POST['refresh'])) {
     $procesaBoton4 = false;
     $procesaBoton5 = false;
 }
-
-
-
-
-
 
 ?>
 <!--Menú navegación-->
@@ -125,7 +247,7 @@ if (isset($_POST['refresh'])) {
         <li>Generación de boletines de notas en función de la evaluación seleccionada en una lista desplegable. <button type="submit" name="submit5"> Mostrar</button></li>
     </ol>
     <button type="submit" name="refresh"> Limpiar</button>
-    <br>
+    <br><br>
 
     <!--Primer botón-->
     <?php
@@ -134,10 +256,10 @@ if (isset($_POST['refresh'])) {
         echo ("<table style=\"border: 1px solid black;\">");
         echo ("<tr><th rowspan=2>Alumnos</th><th colspan=3>DWES</th><th colspan=3>DWEC</th><th colspan=3>DIW</th><th colspan=3>DAW</th><th colspan=3>HLC</th><th colspan=3>EIE</th></tr>");
         echo ("<tr>");
-        for ($i=1; $i < 7; $i++) { 
+        for ($i = 1; $i < 7; $i++) {
             echo "<th>1ª Eval.</th><th>2ª Eval.</th><th style='background-color:grey'>Media</th>";
         };
-        echo ("</tr>");    
+        echo ("</tr>");
         echo ("<tr>");
         foreach ($students as $array) {
 
@@ -146,8 +268,7 @@ if (isset($_POST['refresh'])) {
                 //Nombres alumnos
                 if ($modulos == "Nombre") {
                     echo "<td>$value</td>";
-                    
-                }else {
+                } else {
                     foreach ($value as $notas) {
                         echo "<td>$notas</td>";
                         $media = ($media + $notas);
@@ -163,8 +284,6 @@ if (isset($_POST['refresh'])) {
     }
 
     ?>
-
-
     <style type="text/css">
         table {
             margin: 8px;
@@ -187,8 +306,18 @@ if (isset($_POST['refresh'])) {
             font-weight: bolder;
         }
     </style>
-
-
-
+    <!--Segundo botón-->
+    <?php 
+    if ($procesaBoton2) {
+    
+        echo "El número mayor de aprobados es " . $highestMark;
+        echo "<br><br>";
+        echo "Módulo/s: ";
+        foreach ($highestMarksArray as $key) {
+            echo $key . ", ";
+        }
+    
+    }?>
+   
 
 </form>
