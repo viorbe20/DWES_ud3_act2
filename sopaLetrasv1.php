d453b036b2de2e625200a8feb82a88db1ac22519
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2022-04-05 18:26:03 +0200

    feat:recorre las casillas del array donde queremos colocar la palabra

--- sopaLetrasv1.php
+++ sopaLetrasv1.php
@@ -11,6 +11,10 @@ $text = 0;
 $direction = array("+", "-", "=");
 $rowDirection = "";
 $columnDirection = "";
+//Variable que contiene la forma de recorrer el array
+$loopRow = "";
+$loopColumn = "";
+$n = 1;
 
 //Array de palabras
 $wordsArray = array("MADRID", "LONDRES", "ROMA");
@@ -36,12 +40,15 @@ $wordLength = count($currentWord);
 //var_dump($currentWord);
 
 //Array con 
-for ($i = 0; $i < LENGTHBOARD; $i++) {
-    //echo "<br>";
-    for ($j = 0; $j < LENGTHBOARD; $j++) {
-        $boardArray[$i][$j] = $text;
+for ($i = 0; $i <= LENGTHBOARD; $i++) {
+    echo "<br>";
+    for ($j = 0; $j <= LENGTHBOARD; $j++) {
+$text = "|".$i.$j."|";
+        echo $boardArray[$i][$j] = $text;
     }
 }
+
+$boardArray[0][0] = "*";
 
 $validNumber = "true";
 do {
@@ -59,18 +66,22 @@ do {
 
     //Según la dirección de la línea, calculamos la posición de la línea de la última letra.
     //Controlamos que no se salga del tablero generando números hasta que cuadre
+    //Cargamos también la $i++ del recorrido posterior para saber si tenemos que sumar 1, -1 o 0
     do {
         $rowDirection = $direction[rand(0, 2)];
         //echo "<br>directionR" . $rowDirection;
         switch ($rowDirection) {
             case '+':
                 $lastLR = $firstLR + ($wordLength - 1);
+                $loopRow = "+1"; 
                 break;
             case '-':
                 $lastLR = $firstLR - ($wordLength - 1);
+                $loopRow = "-1"; 
                 break;
             case '=':
                 $lastLR = $firstLR;
+                $loopRow = "+0"; 
                 break;
         }
         //echo "<br>lastLR" . $lastLR;
@@ -78,23 +89,66 @@ do {
 
     //Según la dirección de la columna, calculamos la posición de columna de la última letra
     //Controlamos que no se salga del tablero generando números hasta que cuadre
+    //Cargamos también la $i++ del recorrido posterior para saber si tenemos que sumar 1, -1 o 0
     do {
-        $columnDirection = $direction[rand(0, 2)];
-        echo "<br>firstLC=>" . $firstLC;
-        echo "<br>directionColumn=>" . $columnDirection;
+
+        //Verificamos que al menos una coordenada se desplaza
+        do {
+            $columnDirection = $direction[rand(0, 2)];
+        } while ($columnDirection == "=" && $rowDirection == "=");
+
+        
+        //echo "<br>firstLC=>" . $firstLC;
+        //echo "<br>directionColumn=>" . $columnDirection;
+        //echo "<br>rowColumn=>" . $rowDirection;
         switch ($columnDirection) {
             case '+':
                 $lastLC = $firstLC + ($wordLength - 1);
+                $loopColumn = "1"; 
                 break;
             case '-':
                 $lastLC = $firstLC - ($wordLength - 1);
+                $loopColumn = "-1"; 
                 break;
             case '=':
                 $lastLC = $firstLC;
+                $loopColumn = "0"; 
                 break;
         }
-        echo "<br>lastLC=>" . $lastLC;
+        //echo "<br>lastLC=>" . $lastLC;
     } while ($lastLC > LENGTHBOARD || $lastLC < 0);
+
+    //Recorremos array y comporbamos cada cuadrado
+    $wordLength = 6;
+   for ($index=0; $index < $wordLength; $index++) { 
+    
+    echo "<br>posicion=>" . $boardArray[$firstLR][$firstLC];
+    
+    if ($rowDirection == "+") {
+        $firstLR = $firstLR + 1;
+    } else if ($rowDirection == "-"){
+        $firstLR = $firstLR - 1;
+    }
+
+    if ($columnDirection == "+") {
+        $firstLC = $firstLC + 1;
+    } else if ($columnDirection == "-"){
+        $firstLC = $firstLC - 1;
+    }
+    
+   }
+
+   
+
+    // for ($i = $firstLR; $i = $lastLR; $loopRow++) {
+    //     echo "<br>";
+    //     for ($j = $firstLC; $j = $lastLC; $loopColumn++) {
+    //         //echo $boardArray[$i][$j];
+    //         echo "<br>" . $i;
+    //         echo "<br>" . $j;
+    //     }
+    // }
+
 
 
     //Creamos el array de la última letra con sus coordenadas y le asignamos la letra 
