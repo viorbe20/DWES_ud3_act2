30012a13512313570748011d8f6050b1a7da3a04
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2022-04-06 19:13:59 +0200

    refact: falta coincidencia entre palabrasa

--- sopaLetrasv1.php
+++ sopaLetrasv1.php
@@ -3,30 +3,28 @@
 DEFINE("LENGTHBOARD", 9);
 //Array que contiene los datos de la tabla
 $boardArray = array();
-//Inicia que controla la búsqueda de casilla 
-$validNumber = "";
-//Contenido de cada casilla
-$text = 0;
 //Dirección que va a tomar la palabra: = igual, + suma y - resta
 $direction = array("+", "-", "=");
 $rowDirection = "";
 $columnDirection = "";
-
 $sameLetter = false;
 
 //Array de palabras
-$wordsArray = array("MADRID", "LONDRES");
+$capitalsArray = array("MADRID", "LONDRES", "PARIS");
+$alphabet = array("a", "b", "c", "d", "e", "f");
+$capitalsArrayLenght = count($capitalsArray);
+//$wordsFinished = false;
 //Array que rellenaremos con los datos de cada palabra una vez colocadas y que usaremos para una comprobación final
-$capitalsArray = array();
+$capitalsDataArray = array();
 //array_push($capitalsArray, array("Nombre"=>"MADRID"));
 
 //Contiene la palabra que se vaya a colocar en ese momento. Cada letra ocupa una posición del array
 $currentWord = array();
-$wordLength = "";
+$currentWordLength = "";
 
 //Contiene la posición de la primera y la última letra
-$firstLetter = array();
-$lastLetter = array();
+//$firstLetter = array();
+//$lastLetter = array();
 
 //Contiene coordenadas de primera y última letra
 $firstLR = "";
@@ -35,175 +33,154 @@ $lastLR = "";
 $lastLC = "";
 $row = "";
 $column = "";
-
+$wordSet = true;
 //Indica cuando se ha colocado una palabra y poder pasar a la siguiente
 $wordSet = false;
+$letterSet = false;
 
-//Array con 
+//Array inicial
 for ($i = 0; $i <= LENGTHBOARD; $i++) {
-    echo "<br><br>";
+    //echo "<br><br>";
     for ($j = 0; $j <= LENGTHBOARD; $j++) {
-        $text = $i . $j . " ";
-        echo "<span class='square1'>" . $boardArray[$i][$j] = $text . "</span>";
+        $boardArray[$i][$j] = "0";
     }
 }
 
-$validNumber = "true";
 do {
+    //Seleccionamos siempre la primera capital del array.
+    //Cuando se coloca, se elimina del array
+    $key = $capitalsArray[0];
 
-    //Seleccionamos una capital y colocamos en la sopa. 
-    //Repetimos el mismo proceso con todas hasta que están todas colocadas.
-    foreach ($wordsArray as $key) {
+    //Creamos fila y columna inicial para la primera letra de palabra actual
+    $firstLR = rand(0, 9);
+    $firstLC = rand(0, 9);
 
-        //Creamos fila y columna inicial para la primera letra de palabra actual
-        $firstLR = rand(0, 9);
-        $firstLC = rand(0, 9);
+    //Cargamos en una variable la longitud de la palabra
+    //Extrae la palabra del array y la separa en letras dentro de un array
+    $currentWord = str_split($key);
+    $currentWordLength = count($currentWord);
 
-        //Cargamos en una variable la longitud de la palabra
-        //Extrae la palabra del array y la separa en letras dentro de un array
-        $currentWord = str_split($key);
-        $wordLength = count($currentWord);
+    //Según la dirección de la línea, calculamos la posición de la línea de la última letra.
+    //Controlamos que no se salga del tablero generando números hasta que cuadre
+    do {
+        $rowDirection = $direction[rand(0, 2)];
+        //echo "<br>Dirección  de la fila => " . $rowDirection;
+        switch ($rowDirection) {
+            case '+':
+                $lastLR = $firstLR + ($currentWordLength - 1);
+                break;
+            case '-':
+                $lastLR = $firstLR - ($currentWordLength - 1);
+                break;
+            case '=':
+                $lastLR = $firstLR;
+                break;
+        }
+    } while ($lastLR > LENGTHBOARD || $lastLR < 0);
 
-        //Según la dirección de la línea, calculamos la posición de la línea de la última letra.
-        //Controlamos que no se salga del tablero generando números hasta que cuadre
+    //Según la dirección de la columna, calculamos la posición de columna de la última letra
+    //Controlamos que no se salga del tablero generando números hasta que cuadre
+    do {
+
+        //Verificamos que al menos una coordenada se desplaza. Las dos no pueden ser =
         do {
-            $rowDirection = $direction[rand(0, 2)];
-            //echo "<br>Dirección  de la fila => " . $rowDirection;
-            switch ($rowDirection) {
-                case '+':
-                    $lastLR = $firstLR + ($wordLength - 1);
-                    break;
-                case '-':
-                    $lastLR = $firstLR - ($wordLength - 1);
-                    break;
-                case '=':
-                    $lastLR = $firstLR;
-                    break;
+            $columnDirection = $direction[rand(0, 2)];
+        } while ($columnDirection == "=" && $rowDirection == "=");
+        //echo "<br>Direction  de la columna => " . $columnDirection;
+
+        switch ($columnDirection) {
+            case '+':
+                $lastLC = $firstLC + ($currentWordLength - 1);
+                break;
+            case '-':
+                $lastLC = $firstLC - ($currentWordLength - 1);
+                break;
+            case '=':
+                $lastLC = $firstLC;
+                break;
+        }
+    } while ($lastLC > LENGTHBOARD || $lastLC < 0);
+
+    echo ('<br><br>Capital seleccionada=> ' . $key);
+    echo '<br>' . $key . " inicia en " . $firstLR . $firstLC;
+    echo '<br>' . $key . " termina en " . $lastLR . $lastLC;
+    echo '<br>Longitud de ' . $key . " => " . $currentWordLength;
+
+    //Cargamos datos de capital y coordenadas en el array de verificación
+    $capitalsDataArray = array("Nombre" => $key, "Empieza" => $firstLR . $firstLC, "Acaba" => $lastLR . $lastLC);
+    //Comprobación colocación array
+    // foreach ($capitalsDataArray as $key => $value) {
+    //     echo ('<br>' . $key . ": " . $value);
+    // }
+
+    //Cargamos estas variables con la posición de la letra incial y 
+    //las usaremos como índices para recorrer el array inicial
+    $row = $firstLR;
+    $column = $firstLC;
+
+    //Recorremos la palabra para ir comprobando cada letra
+    do {
+
+        foreach ($currentWord as $key => $value) {
+            //Siempre comprobamos la primera letra
+            //Cuando se coloque la eliminamos del array
+            $letter = $value;
+            echo ('<br><br>Letra actual => ' . $letter);
+            echo ('<br>Contenido => ' . $boardArray[$row][$column]);
+
+            //Si hay letra, comprobamos si es la misma
+             //Siempre inicia en false
+            $sameLetter = false;
+            if ($boardArray[$row][$column] == $letter) {
+                echo ('<br>Misma letra' . $letter);
+                $sameLetter = true;
             }
-        } while ($lastLR > LENGTHBOARD || $lastLR < 0);
 
-        //Según la dirección de la columna, calculamos la posición de columna de la última letra
-        //Controlamos que no se salga del tablero generando números hasta que cuadre
-        do {
+            //Si el contenido de la sopa es =, o la letra es la misma, colocamos letra
+            if (($boardArray[$row][$column] == "0") || $sameLetter) {
+                echo ('<br>Contiene numérico => ' . $boardArray[$row][$column]);
+                //Sobreescribimos
+                $boardArray[$row][$column] =  $letter;
+                echo ('<br>Nuevo contenido => ' . $boardArray[$row][$column]);
 
-            //Verificamos que al menos una coordenada se desplaza. Las dos no pueden ser =
-            do {
-                $columnDirection = $direction[rand(0, 2)];
-            } while ($columnDirection == "=" && $rowDirection == "=");
-            //echo "<br>Direction  de la columna => " . $columnDirection;
-
-            switch ($columnDirection) {
-                case '+':
-                    $lastLC = $firstLC + ($wordLength - 1);
-                    break;
-                case '-':
-                    $lastLC = $firstLC - ($wordLength - 1);
-                    break;
-                case '=':
-                    $lastLC = $firstLC;
-                    break;
-            }
-        } while ($lastLC > LENGTHBOARD || $lastLC < 0);
-
-        echo ('<br><br>Capital seleccionada=> ' . $key);
-        echo '<br>' . $key . " inicia en " . $firstLR . $firstLC;
-        echo '<br>' . $key . " termina en " . $lastLR . $lastLC;
-        echo '<br>Longitud de ' . $key . " => " . $wordLength;
-
-        //Cargamos datos de capital y coordenadas en el array de verificación
-        $capitalsArray = array("Nombre" => $key, "Empieza" => $firstLR . $firstLC, "Acaba" => $lastLR . $lastLC);
-        //Comprobación colocación array
-        // foreach ($capitalsArray as $key => $value) {
-        //     echo ('<br>' . $key . ": " . $value);
-        // }
-
-        //Recorremos array inicial y leemos lo que hay en cada casilla
-        //Lo hacemos hasta que consigamos confirmar el sitio la palabra
-        do {
-            $row = $firstLR;
-            $column = $firstLC;
-
-            for ($index = 0; $index < $wordLength; $index++) {
-                //echo ('<br><br>Contenido de posición ' . $firstLR . $firstLC . " => " . $boardArray[$firstLR][$firstLC]);
-
-                //Recorremos la palabra para ir comprobando cada letra
-                foreach ($currentWord as $value => $letter) {
-                    //echo ('<br>Letra actual => ' . $letter);
-
-                    //Si hay letra, comprobamos si es la misma
-                    if ($boardArray[$row][$column] == $letter) {
-                        echo ('<br>Misma letra' . $letter);
-                        $sameLetter = true;
-                    }
-
-                    echo ('<br><br>Contenido => ' . $boardArray[$row][$column]);
-                    //Si el contenido de la sopa es numérico, o la letra es la misma, colocamos letra
-                    if (!is_int($boardArray[$row][$column])) {
-                        echo ('<br>Contiene numérico => ' . $boardArray[$row][$column]);
-                        //Sobreescribimos
-                        $boardArray[$row][$column] =  $letter . "-";
-                        echo ('<br>Nuevo contenido => ' . $boardArray[$row][$column]);
-
-                        //Seguimos recorriendo la palabra hasta el final y haciendo las mismas comprobaciones
-                        if ($rowDirection == "+") {
-                            $row = $row + 1;
-                        } else if ($rowDirection == "-") {
-                            $row = $row - 1;
-                        }
-
-                        if ($columnDirection == "+") {
-                            $column = $column + 1;
-                        } else if ($columnDirection == "-") {
-                            $column = $column - 1;
-                        }
-                    } else {
-                        echo ('<br>Nooooooo contiene numérico => ' . $boardArray[$row][$column]);
-                    }
-
-
-
-                    // if ($sameLetter) {
-                    //     echo ('<br>Misma letra => ' . $boardArray[$row][$column] . " y " . $letter);
-                    //     $sameLetter = false;
-                    // }
-                    // if (is_numeric($boardArray[$row][$column] || $sameLetter)) {
-                    //     //Volvemos a cambiar la bandera para la próxima vuelta
-                    //     $sameLetter = false;
-                    //     
-                    //     echo ('<br><br>Contenido de posición cambiado ' . $row . $column . " => " . $boardArray[$row][$column]);
-
-
-                    // } else {
-                    //     $wordSet = false;
-                    // }
+                //Seguimos recorriendo la palabra hasta el final y haciendo las mismas comprobaciones
+                if ($rowDirection == "+") {
+                    $row = $row + 1;
+                } else if ($rowDirection == "-") {
+                    $row = $row - 1;
                 }
 
-                //En la última lectura del for hacemos que se salga
-                if ($index = ($wordLength - 1)) {
-                    $wordSet = true;
+                if ($columnDirection == "+") {
+                    $column = $column + 1;
+                } else if ($columnDirection == "-") {
+                    $column = $column - 1;
                 }
+                //Si se coloca eliminamos la letra del array 
+                array_splice($currentWord, 0, 1);
+                echo ('<br>letra colocada');
+            } else {
+                echo ('<br>Nooooooo se puede colocar');
+                $wordSet = false; 
+                //Si no la coloca vuelve a empezar al proceso con la misma palabra
             }
-        } while (!$wordSet);
+        }
+
+    } while ($wordSet);
 
 
+    //Si sale del array anterior, es que se ha colocado la palabra por lo que se elimina del array
+    array_splice($capitalsArray, 0, 1);
+} while (count($capitalsArray) > 0);
 
-        //Cambiamos para que empiece con false en la siguiente capital
-        $wordSet = false;
 
-        //Aquí ya hemos comprobado que la palabra tiene un espacio
-        //echo ('<br>' . $key . " colocada.");
-    } //foreach capitales
-
-    $validNumber = false;
-} while ($validNumber);
-
-//Imprimimos array relleno
-echo ('<br><br>');
 for ($i = 0; $i <= LENGTHBOARD; $i++) {
-    echo "<br>";
+    echo "<br><br>";
     for ($j = 0; $j <= LENGTHBOARD; $j++) {
-
-        echo "<strong>" . $boardArray[$i][$j] . "</strong>";
+        if ($boardArray[$i][$j] != 0) {
+            echo "<span style='color:red'>" . $boardArray[$i][$j] . "</span>";
+        } else {
+            echo $boardArray[$i][$j] = $alphabet[rand(0, 5)];
+        }
     }
 }
 
